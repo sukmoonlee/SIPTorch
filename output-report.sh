1648c80b420e264ce9bf2ada6c7c61c30acbb3d4
Sukmoon Lee <smlee@sukmoonlee.com>
2023-07-19 18:05:20 +0900

    o add a result report script based on output log file

---
+++ output-report.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+################################################################################
+# Generate Report based on output file
+# 2023.07.19 created by smlee@sk.com
+################################################################################
+
+filename=$1
+if [ ! -f "$filename" ] ; then
+	echo "error: file is not exist ($filename)"
+	echo ""
+	echo "Usage: $0 [filename]"
+	exit 1
+fi
+
+flag=0
+while read -r i_line
+do
+	#echo "$flag $i_line"
+	if [ "${i_line:0:9}" == "### Test:" ] ; then
+		test_title=${i_line:10}
+	fi
+	if [ "${i_line:0:11}" == "- Category:" ] ; then
+		test_category=${i_line:12}
+	fi
+	if [ "${i_line:0:5}" == "- ID:" ] ; then
+		test_id=${i_line:6}
+		test_id=${test_id//\`/}
+	fi
+	if [ "${i_line:0:11}" == "- Response:" ] ; then
+		flag=1
+	fi
+	if [ "$flag" == "0" ] ; then continue; fi
+	if [ "$flag" == "1" ] && [ "${i_line:0:3}" == "\`\`\`" ] ; then flag=2; test_result=; continue; fi
+	if [ "$flag" == "2" ] && [ "$test_result" == "" ] ; then test_result="$i_line"; fi
+	if [ "$flag" == "2" ] && [ "${i_line:0:3}" == "\`\`\`" ] ; then
+		flag=0
+		if [ "${test_result:0:3}" == "\`\`\`" ] ; then test_result=; fi
+		echo "$test_category|$test_id|$test_title|$test_result"
+	fi
+done < <(cat "$filename")
